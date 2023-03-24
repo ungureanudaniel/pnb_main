bb3e5db3b444b29b4bb60392683e3b05183e8d8e
Dani <36001908+ungureanudaniel@users.noreply.github.com>
2023-03-24 10:55:11 +0200

    9th commit, reset buggy database

--- pnb_main/settings.py
+++ pnb_main/settings.py
@@ -90,24 +90,16 @@ WSGI_APPLICATION = 'pnb_main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if DEVELOPMENT == True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
-    }
+}
 
 
 # Password validation
--- services/urls.py
+++ services/urls.py
@@ -8,44 +8,7 @@ blogsearch_view, eventlist_view, map_coming_soon, terms_view, infopoints_view
 from django.conf.urls.static import static
 from users.views import user_logout
 
-if settings.DEVELOPMENT == True:
-    urlpatterns = [
-        path('logout/', user_logout, name='signout'),
-        path('', home, name="home"),
-        path('contact', contacts_view, name="contact"),
-        path('coming-soon', coming_soon, name="coming-soon"),
-        path('gallery', gallery, name="gallery"),
-        path('team', team, name="team"),
-        path('wildlife', wildlife, name="wildlife"),
-        path('flora', flora, name="flora"),
-        path('history', history, name="history"),
-        path('videos', video_view, name="videos"),
-        path('_(frequently-asked-questions)', faq_view, name="faq"),
-        path('theme-trails', theme_trails, name="theme-trails"),
-        path('public-documents', public_docs, name="public-docs"),
-        path('ticket-information', ticket_info, name="ticket-info"),
-        path('privacy_information', privacy_view, name="privacy_info"),
-        path('terms-conditions', terms_view, name="terms"),
-        path('info-points', infopoints_view, name="infopoints"),
-
-
-        #------------map urls-------------------------------
-        path('park-map', map_coming_soon, name="map_coming_soon"),
-        #------------blog urls------------------------------
-        path('blog', bloglist_view, name="blog"),
-        path('blog/<slug:slug>/', PostDetailView.as_view(), name='blog-details'),
-        path('blog-search/q/', blogsearch_view, name="blogsearch"),
-        #--------events urls-----------------------------
-        path('events', eventlist_view, name="events"),
-
-        #---------add content urls---------------------
-        # path('add-attraction', add_attraction, name="add_attraction"),
-        path('add-testimonial', add_testimonial, name="add_testimonial"),
-        ]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns = [
+urlpatterns = [
         #-------Authentication----------------
         path('logout/', user_logout, name='signout'),
         #-------Visitor urls------------------
@@ -77,11 +40,10 @@ else:
         path('events', eventlist_view, name="events"),
 
         #---------add content urls---------------------
-        # path('add-attraction', add_attraction, name="add_attraction"),
         path('add-testimonial', add_testimonial, name="add_testimonial"),
 
 
 
-        ]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
