f52c530633c15d1113a3cc40d263ac291250d338
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-04-05 17:01:50 +0300

    purged part of css and fixed translation for a phrase in public docs

--- pnb_main/settings.py
+++ pnb_main/settings.py
@@ -84,6 +84,13 @@ TEMPLATES = [
         },
     },
 ]
+# 
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+#         'LOCATION': '127.0.0.1:8000',
+#     }
+# }
 
 WSGI_APPLICATION = 'pnb_main.wsgi.application'
 
