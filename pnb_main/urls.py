7e8287ebd0524239fffc14d543111b55fc42d8ca
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-04-13 13:46:06 +0300

    Fixed sitemaps url and robots url

--- pnb_main/urls.py
+++ pnb_main/urls.py
@@ -7,18 +7,28 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 from django.contrib.sitemaps.views import sitemap
-from services.sitemaps import StaticViewSitemap
+from django.views.generic import TemplateView
+from services.sitemaps import *
 
 
 sitemaps = {
+    'blogpost':BlogPostSitemap,
+    'attraction-category':AttractionCategorySitemap,
+    'flora-category':FloraCategorySitemap,
+    'flora':FloraSitemap,
+    'wildlife':WildlifeSitemap,
+    'wildlife-category':WildlifeCategorySitemap,
+    'events': EventSitemap,
+    'team': TeamSitemap,
     'static': StaticViewSitemap,
-    # 'snippet': SnippetSitemap
+
 }
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     # path('__debug__/', include('debug_toolbar.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path("robots.txt", TemplateView.as_view(template_name="services/robots.txt", content_type="text/plain"),),
 ]
 urlpatterns += i18n_patterns(
 
--- services/sitemaps.py
+++ services/sitemaps.py
@@ -1,6 +1,7 @@
 from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
-from .models import BlogPost, AttractionCategory, Event, Team
+from .models import BlogPost, AttractionCategory, Event, Team, FloraCategory, Flora,\
+Wildlife, WildlifeCategory
 
 class BlogPostSitemap(Sitemap):
     changefreq = "weekly"
@@ -21,7 +22,42 @@ class AttractionCategorySitemap(Sitemap):
         return AttractionCategory.objects.all()
     def location(self,obj):
         return '/%s' % (obj.name)
+class FloraCategorySitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
 
+    def items(self):
+        return FloraCategory.objects.all()
+    def location(self,obj):
+        return '/%s' % (obj.name)
+class FloraSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return Flora.objects.all()
+    def location(self,obj):
+        return '/%s' % (obj.name)
+class WildlifeSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return Wildlife.objects.all()
+    def location(self,obj):
+        return '/%s' % (obj.name)
+class WildlifeCategorySitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return WildlifeCategory.objects.all()
+    def location(self,obj):
+        return '/%s' % (obj.name)
 class EventSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
