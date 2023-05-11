bec1bb81265a46f947cba3d294e973ee67beb13b
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-05-11 08:48:55 +0300

    Erase payment models

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
-from .models import Ticket, Payment
+# from .models import Ticket, Payment
 import random, string
 from django.utils.text import slugify
 #----------generate unique code for email subscription conf--------------------
@@ -31,35 +31,35 @@ def checkout_view(request):
     context = {
 
     }
-    if request.method=='POST':
-        tickets = int(request.POST.get('tickets_nr'))
-        price = float(request.POST.get('total_price'))
-        print("tickets:",tickets)
-        fname = request.POST.get('first-name')
-        lname = request.POST.get('last-name')
-        phone = request.POST.get('phone-number')
-        email = request.POST.get('email')
-        address = request.POST.get('address')
-        county = request.POST.get('county')
-        country = request.POST.get('country-select')
-        city = request.POST.get('city')
-        zip = request.POST.get('zip')
-        note = request.POST.get('notes')
-        code = f"{random_series(3).upper()}"+ " " + f"{random_ticket_nr()}"
-        print(code)
-        #=============save new payment in database ============
-        try:
-            new_pay = Payment(payment_code=code, sum=price, buyer_fname=fname, buyer_lname=lname, phone=phone, email=email, address=address, county=county, country=country, city=city,zip=zip, notes=note)
-            new_pay.slug = slugify(code)
-            new_pay.save()
-        except Exception as e:
-            messages.warning(request, _(f"Warning! Payment functionalities disabled and under maintenance."))
-        #=============loop through the number of tickets and generate tickets pdf========
-        for ticket in range(1, tickets+1):
-            new_ticket = Ticket()
-        context.update({
-            "tickets": tickets,
-            "price": price,
-        })
+    # if request.method=='POST':
+    #     tickets = int(request.POST.get('tickets_nr'))
+    #     price = float(request.POST.get('total_price'))
+    #     print("tickets:",tickets)
+    #     fname = request.POST.get('first-name')
+    #     lname = request.POST.get('last-name')
+    #     phone = request.POST.get('phone-number')
+    #     email = request.POST.get('email')
+    #     address = request.POST.get('address')
+    #     county = request.POST.get('county')
+    #     country = request.POST.get('country-select')
+    #     city = request.POST.get('city')
+    #     zip = request.POST.get('zip')
+    #     note = request.POST.get('notes')
+    #     code = f"{random_series(3).upper()}"+ " " + f"{random_ticket_nr()}"
+    #     print(code)
+    #     #=============save new payment in database ============
+    #     try:
+    #         new_pay = Payment(payment_code=code, sum=price, buyer_fname=fname, buyer_lname=lname, phone=phone, email=email, address=address, county=county, country=country, city=city,zip=zip, notes=note)
+    #         new_pay.slug = slugify(code)
+    #         new_pay.save()
+    #     except Exception as e:
+    #         messages.warning(request, _(f"Warning! Payment functionalities disabled and under maintenance."))
+    #     #=============loop through the number of tickets and generate tickets pdf========
+    #     for ticket in range(1, tickets+1):
+    #         new_ticket = Ticket()
+    #     context.update({
+    #         "tickets": tickets,
+    #         "price": price,
+    #     })
 
     return render(request, template, context)
