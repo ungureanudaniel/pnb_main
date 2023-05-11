02e3ff3c3b43493408fe5b10ba32ac54b586f740
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-05-11 17:26:42 +0300

    Added ticket and payment views

--- payments/admin.py
+++ payments/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-# from .models import Payment, Ticket
+from .models import Payment, Ticket
 
-# class TicketAdmin(admin.ModelAdmin):
-#     fields = ['buyer_fname', 'buyer_lname', 'ticket_series', 'ticket_nr', 'start_date', 'expiry']
-#
-# class PaymentAdmin(admin.ModelAdmin):
-#     fields = ['buyer_fname','buyer_lname', 'payment_code', 'sum', 'phone', 'email', 'address', 'text_de', 'featured']
-#
-# admin.site.register(Ticket, TicketAdmin)
-# admin.site.register(Payment, PaymentAdmin)
+class TicketAdmin(admin.ModelAdmin):
+    fields = ['buyer_fname', 'buyer_lname', 'ticket_series', 'ticket_nr', 'start_date', 'expiry']
+
+class PaymentAdmin(admin.ModelAdmin):
+    fields = ['buyer_fname','buyer_lname', 'payment_code', 'sum', 'phone', 'email', 'address', 'text_de', 'featured']
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Payment, PaymentAdmin)
