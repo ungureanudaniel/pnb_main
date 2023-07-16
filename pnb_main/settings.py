acd4ac8833a5826b78b7270a399425c39d6f032e
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-07-16 10:49:16 +0300

    Set debug value to the one in env file

--- pnb_main/settings.py
+++ pnb_main/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')== 'True'
 ALLOWED_HOSTS = []
 if os.getenv('ALLOWED_HOSTS'):
     ALLOWED_HOSTS.extend(os.getenv('ALLOWED_HOSTS').split(','))
