f6946526a796e3fb722374553119c57c9092f420
ungureanudaniel <ungureanu.daniel86@gmail.com>
2023-09-01 15:03:27 +0300

    Fixed payment_code to payment_id in admin.py

--- payments/admin.py
+++ payments/admin.py
@@ -5,7 +5,7 @@ class TicketAdmin(admin.ModelAdmin):
     fields = ['buyer_fname', 'buyer_lname', 'ticket_series', 'ticket_nr', 'start_date', 'expiry']
 
 class PaymentAdmin(admin.ModelAdmin):
-    fields = ['buyer_fname','buyer_lname', 'payment_code', 'quantity', 'price', 'currency', 'phone', 'email', 'address', 'county', 'country', 'city', 'zip', 'notes', ]
+    fields = ['buyer_fname','buyer_lname', 'payment_id', 'quantity', 'price', 'currency', 'phone', 'email', 'address', 'county', 'country', 'city', 'zip', 'notes', ]
 
 admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Payment, PaymentAdmin)
