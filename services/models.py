3217e541c8a959cef7140cbe128f96c38c70e81f
ungureanudaniel <ungureanu.daniel86@gmail.com>
2024-01-04 11:44:56 +0200

    Edited allowed vehicles model, added area, excluded permit date, repositioned issuance date and expiry date, fixed translations for this data

--- services/models.py
+++ services/models.py
@@ -300,13 +300,14 @@ class AllowedVehicles(models.Model):
     This class creates database tables for each allowed motorized vehicle inside the park
     """
     owner = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000, null=True, blank=True)
+    description = models.TextField(default="...", max_length=1000, null=True, blank=True)
     categ = models.ForeignKey(VehicleCategory, on_delete=models.CASCADE)
     identification_nr = models.CharField(max_length=20)
+    area = models.CharField(max_length=50)
     permit_nr = models.CharField(max_length=50)
-    permit_date = models.DateField()
+    # permit_date = models.DateField()
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(default="2024-12-31")
     slug = models.SlugField(max_length=100, blank=True, null=True, editable=False)
     class Meta:
         verbose_name = _('Allowed Vehicles')
